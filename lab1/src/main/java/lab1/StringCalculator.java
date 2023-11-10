fbfdcd70458cf5c715d56690aacc5a956f4ea2a1
Dmytro Saienko <saenko26051978@gmail.com>
2023-11-10 18:08:46 +0200

    1 step

--- lab1/src/main/java/lab1/StringCalculator.java
+++ lab1/src/main/java/lab1/StringCalculator.java
@@ -1,4 +1,37 @@
 package lab1;
+import java.util.Scanner;
+
 
 public class StringCalculator {
+    public static int Add(String numbers) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        String[] numArr = numbers.split(",");
+
+        if (numArr.length > 2) {
+            throw new IllegalArgumentException("помилка: недійсний рядок");
+        }
+
+        int res = 0;
+        for (String num : numArr) {
+            num = num.trim();
+            try {
+                int x = Integer.parseInt(num);
+                res += x;
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("помилка: недійсний рядок");
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("введіть рядок: ");
+        String input = scanner.nextLine();
+        int res = Add(input);
+        System.out.println("результат: "+res);
+        scanner.close();
+    }
 }
